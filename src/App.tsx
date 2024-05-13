fcba6e2f6901fae2b7e322ccacc07186c49bad08
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-05-13 22:09:39 +0500

    commit -2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   const [backgroundImage, setBackgroundImage] = useState("");
   const galleryImages = [
-    "https://via.placeholder.com/150x300.png?text=Image+1", // Replace these URLs with actual image URLs
+    "https://via.placeholder.com/150x300.png?text=Image+1",
     "https://via.placeholder.com/150x300.png?text=Image+2",
     "https://via.placeholder.com/150x300.png?text=Image+3",
     "https://via.placeholder.com/150x300.png?text=Image+4",
@@ -32,7 +32,26 @@ function App() {
       >
         <p>Hello world!</p>
       </div>
-      <input type="file" onChange={handleImageUpload} />
+      <input
+        style={{
+          display: "none",
+        }}
+        type="file"
+        onChange={handleImageUpload}
+      />
+
+      <button
+        id="upload-button"
+        onClick={() => {
+          const fileInput = document.querySelector(
+            "input[type=file]"
+          ) as HTMLInputElement;
+          fileInput?.click();
+        }}
+      >
+        Upload Image
+      </button>
+
       <div className="gallery">
         {galleryImages.map((image, index) => (
           <img
