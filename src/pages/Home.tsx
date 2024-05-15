e8d3dc19942e252687b110db2bad3a6aa1b746f7
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2024-05-16 02:58:43 +0500

    fix 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function HomeScreen() {
           backgroundImage: "url('/img.jpg')",
           backgroundSize: "cover",
         }}
-        className="h-full mx-4 my-10 flex flex-col items-center bg-gray-900 rounded-2xl pt-[20%] w-[350px] max-w-[350px] md:mx-auto outline outline-[6px] outline-gray-800"
+        className="h-full mx-4 my-10 flex flex-col items-center bg-gray-900 rounded-2xl pt-[25vh] w-[350px] max-w-[350px] md:mx-auto outline outline-[6px] outline-gray-800"
       >
         <div className="w-2 top-10 absolute h-2 my-4 bg-black rounded-full"></div>
         <h1 className="text-center text-2xl md:text-5xl md:py-10 py-4">
